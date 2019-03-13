79e4dfe34538e41412693ef992567399cb385a19
reffymahesya <reffymahesyar@gmail.com>
2019-03-13 17:05:46 +0700

    Ecercise 06, 07, 08, 09

--- Array/Exercise 04 - Tantangan Array 3 (Array Join, Split, Slice, Splice, Sort)/Exercise 4.js
+++ Array/Exercise 04 - Tantangan Array 3 (Array Join, Split, Slice, Splice, Sort)/Exercise 4.js
@@ -1,7 +1,7 @@
 var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
 
 function dataHandling2(input){
-	input.splice(1, 1, "Roman Alamsyah Eshawary")
+  input.splice(1, 1, "Roman Alamsyah Eshawary")
   input.splice(2, 1, "Provinsi Bandar Lampung")
   input.splice(5, 0, "Pria", "SMA Internasional Metro")
   console.log(input)
@@ -10,13 +10,33 @@ function dataHandling2(input){
   var rubah = pisah.join("-")
 
   switch(pisah[1]){
-    case '05':
-    pisah[1] = 'Mei'
+    case '01': console.log('Januari')
+    break
+    case '02': console.log('Februari')
+    break
+    case '03': console.log('Maret')
+    break
+    case '04': console.log('April')
+    break
+    case '05': console.log('Mei')
+    break
+    case '06': console.log('Juni')
+    break
+    case '07': console.log('Juli')
+    break
+    case '08': console.log('Agustus')
+    break
+    case '09': console.log('September')
+    break
+    case '10': console.log('Oktober')
+    break
+    case '11': console.log('November')
+    break
+    case '12': console.log('Desember')
     break
   }
-  console.log(pisah[1])
 
-  var tukar = pisah.sort(function(input, tukar){return tukar-input});
+  var tukar = pisah.sort(function(value1, value2){return value2 - value1});
   console.log(tukar)
 
   console.log(rubah)
